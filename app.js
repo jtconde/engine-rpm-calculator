58060e0a07102769abb07947ea7d6c14ba01df61
Julian Conde <jconde@OGC-jconde.local>
2019-07-10 16:48:06 -0400

    Updated Javascript

--- app.js
+++ app.js
@@ -16,10 +16,16 @@ function calculate() {
     transmissionSelection = document.getElementById("transmissionSelect").value;
     gearSelection = document.getElementById("gearSelect").value;
     gearRatio = document.getElementById("transGearRatio").value;
+    tireUno = document.getElementById("tireOne").value;
+    tireDos = document.getElementById("tireTwo").value;
+    tireTres = document.getElementById("tireThree").value;
+    speed = document.getElementById("speedz").value;
+    rearRatio = tireUno = document.getElementById("rearGearRatio").value;
     
     // Update Results Label 
     document.getElementById("resultsLabel").innerHTML = "Results for " + yearSelection + " Mustang " + trimSelection;
-    // Test Data Line
-    console.log(yearSelection + " Mustang " + trimSelection + " - " + transmissionSelection
-        + " Gear: " + gearSelection + " - " + "Gear Ratio " + gearRatio);
+    // Log Test Data Line
+    console.log(yearSelection + " Mustang " + trimSelection + " -> Transmission: " + transmissionSelection
+        + " -> Gear: " + gearSelection + " -> Gear Ratio: " + gearRatio + " -> Tire Size: " + tireUno + "/" + tireDos + "-" + tireTres
+        + " -> Speed: " + speed + " -> Rear Gear Ratio: " + rearRatio);
 }
